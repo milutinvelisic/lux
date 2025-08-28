f0dbf44b3ecd44a1730e897857f5f956d373cb67
LukaKurtuma <kurtuma4@gmail.com>
2025-08-29 00:14:37 +0200

    test

---
+++ js/footer.js
@@ -0,0 +1,88 @@
+document.getElementById("footer").innerHTML = `
+    <div class="container-lg padding-medium text-white">
+      <div class="row ">
+        <div class="col-lg-4 my-4 ">
+          <div class="footer-menu">
+            <a class="navbar-brand" href="index.html">
+              <img src="images/logo-futer.png" class="logo img-fluid">
+            </a>
+            <p class="text-white mt-4">Hic, dolor soluta, dolores laudantium reprehenderit ullam, similique voluptate
+              omnis modi
+              sit minima illo.</p>
+            <ul class="info list-unstyled mt-4">
+
+            </ul>
+            <div class="social-links mt-4">
+              <ul class="d-flex  list-unstyled gap-2 m-0">
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:facebook-fill"></iconify-icon>
+                  </a>
+                </li>
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:instagram-fill"></iconify-icon>
+                  </a>
+                </li>
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:tiktok-fill"></iconify-icon>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 my-4 ">
+          <div class="footer-menu">
+            <h6 class="text-uppercase text-white fw-bold  mb-4">Izdvajamo</h6>
+            <ul class="menu-list list-unstyled">
+              <li class="menu-item">
+                <a href="index.html" class="footer-link">Početna</a>
+              </li>
+              <li class="menu-item">
+                <a href="aboutus.html" class="footer-link">O nama</a>
+              </li>
+              <li class="menu-item">
+                <a href="gallery.html" class="footer-link">Galerija </a>
+              </li>
+              <li class="menu-item">
+                <a href="#" class="footer-link">Usluge</a>
+              </li>
+              <li class="menu-item">
+                <a href="contact.html" class="footer-link">Kontakt</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 my-4 ">
+          <div class="footer-menu">
+            <h6 class="text-uppercase text-white fw-bold  mb-4">Kontakt</h6>
+            <ul class="info list-unstyled mt-4">
+              <li class="clock text-capitalize mb-2 align-items-center">
+                <svg class="text-white me-1" width="18" height="18">
+                  <use xlink:href="#clock"></use>
+                </svg>00:00 - 24:00
+              </li>
+              <li class="location text-capitalize mb-2 align-items-center">
+                <svg class="text-white me-1" width="18" height="18">
+                  <use xlink:href="#location"></use>
+                </svg>Zrenjaninski Put 84
+              </li>
+
+              <li class="phone text-capitalize mb-2 align-items-center">
+                <svg class="text-white me-1" width="18" height="18">
+                  <use xlink:href="#phone"></use>
+                </svg>060 7171153
+              </li>
+              <li class="email text-capitalize mb-2 align-items-center">
+                <svg class="text-white me-1" width="18" height="18">
+                  <use xlink:href="#email"></use>
+                </svg>newluxevent@gmail.com
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+`;
---
+++ js/header.js
@@ -0,0 +1,51 @@
+document.getElementById("header").innerHTML = `
+      <nav id="header-nav" class="navbar navbar-expand-lg py-4">
+      <div class="container-lg">
+        <a class="navbar-brand" href="index.html">
+          <img src="images/luxlogo.png" class="logo" alt="lux logo" style="width: 100px">
+        </a>
+        <button class="navbar-toggler d-flex d-lg-none order-3 p-2 border-0" type="button" data-bs-toggle="offcanvas"
+          data-bs-target="#bdNavbar" aria-controls="bdNavbar" aria-expanded="false" aria-label="Toggle navigation">
+          <svg class="navbar-icon text-primary">
+            <use xlink:href="#navbar-icon"></use>
+          </svg>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="bdNavbar" aria-labelledby="bdNavbarOffcanvasLabel">
+          <div class="offcanvas-header px-4 pb-0">
+            <a class="navbar-brand" href="index.html">
+              <img src="images/main-logo.png" class="logo">
+            </a>
+            <button type="button" class="btn-close btn-close-black" data-bs-dismiss="offcanvas" aria-label="Close"
+              data-bs-target="#bdNavbar"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul id="navbar"
+              class="navbar-nav text-uppercase justify-content-start justify-content-lg-center align-items-start align-items-lg-center flex-grow-1">
+              <li class="nav-item">
+                <a class="nav-link me-4 active" href="index.html">Početna</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link me-4" href="aboutus.html">O nama</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link me-4 dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                  aria-expanded="false">Usluge</a>
+                <ul class="dropdown-menu animate slide border">
+                  <li><a href="servicesdpd.html" class="dropdown-item">Services <span
+                        class="badge bg-primary">Pro</span></a></li>
+                  <li><a href="servicesdetails.html" class="dropdown-item">Service-Details <span
+                        class="badge bg-primary">Pro</span></a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link me-4" href="gallery.html">Galerija</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link me-4" href="contact.html">Kontakt</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+`;
